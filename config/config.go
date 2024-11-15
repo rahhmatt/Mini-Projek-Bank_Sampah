9031f44b25a372d9fa16e7bf7f3d93eb381c9577
Rahmat Sobirin <rahmatsobirin321@gmail.com>
2024-11-15 19:43:03 +0700

    first commit

---
+++ config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	var err error
+	dsn := "root:rahmat08@tcp(127.0.0.1:3306)/mini_proyek?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+}
